814664d35ba655cdc96de52e93d245a7d160bf1c
Raemyn <Zhekalah@gmail.com>
2026-04-11 15:55:37 +0500

    Роботы

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-
-
-
-import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
-
-
   return (
-   <Home />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
