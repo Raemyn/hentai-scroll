2e81269c96598387dd78b972305ab9b29a5db2db
Raemyn <zhekalah@gmail.com>
2026-05-30 17:51:07 +0500

    тест без stricmod

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import { MantineProvider} from '@mantine/core'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ 
     <MantineProvider  defaultColorScheme="light">
       <App />
     </MantineProvider>
-  </React.StrictMode>
+  
 )
