f602d4901b913aedfe4de120714cc40a5634ab82
Raemyn <zhekalah@gmail.com>
2026-05-30 18:11:30 +0500

    что за бред не уж то блок?

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { MantineProvider} from '@mantine/core'
@@ -6,9 +5,7 @@ import { MantineProvider} from '@mantine/core'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <MantineProvider  defaultColorScheme="light">
       <App />
     </MantineProvider>
-  </React.StrictMode>
 )
